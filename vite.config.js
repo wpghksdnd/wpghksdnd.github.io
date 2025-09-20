56525e8c1637215715611c2bd94f0004065f4079
wpghksdnd <oahfaj@naver.com>
2025-09-20 18:27:39 +0900

    Add .nojekyll file to disable Jekyll processing

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: './',  // 상대 경로로 변경
   plugins: [react()],
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
+        manualChunks: undefined,
+      },
+    },
   },
-  server: {
-    fs: {
-      strict: false
-    }
-  }
 })

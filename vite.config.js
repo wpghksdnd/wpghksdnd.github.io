576d4dd2d900e9d59c10865ddc11388d8052c624
wpghksdnd <oahfaj@naver.com>
2025-09-20 17:21:15 +0900

    213

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://github.com/wpghksdnd/my-portfolio', 
+  base: 'https://wpghksdnd.github.io/', 
   plugins: [react()],
 })

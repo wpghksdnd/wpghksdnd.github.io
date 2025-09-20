35c9fd2d8e4aff329286983210d9d82457df81a6
wpghksdnd <oahfaj@naver.com>
2025-09-20 15:25:07 +0900

    수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+// App.jsx
+import { useState, useEffect } from "react";
+import Intro from "./components/Intro";
+import Main from "./components/Main";
+import Projects from "./components/Projects";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  const [showIntro, setShowIntro] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowIntro(false), 2000); // 2초 후 Intro 종료
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div style={{ width: "100%", minHeight: "100vh" }}>
+      {showIntro ? <Intro /> : (
+        <>
+          <Main />
+          <Projects />
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;

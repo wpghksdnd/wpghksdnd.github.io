befe1b1ac31e337e7ac2f97cdd8d9310d47e6a17
wpghksdnd <oahfaj@naver.com>
2025-09-20 17:09:19 +0900

    123123

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// 레포지토리 이름이 "portfolio"이면 base를 "/portfolio/"
 export default defineConfig({
+  base: 'https://wpghksdnd.github.io/', // GitHub Pages URL
   plugins: [react()],
-  base: '/wpghksdnd.github.io/', // ⚠ 여기에 레포 이름 넣기
-});
+})
